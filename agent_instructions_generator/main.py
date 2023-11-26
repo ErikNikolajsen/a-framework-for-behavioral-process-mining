3024f1a36a58bca23e994be434ec26716e8a8948
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2023-11-26 12:12:11 +0100

    AIG output update

--- agent_instructions_generator/main.py
+++ agent_instructions_generator/main.py
@@ -78,16 +78,16 @@ def run_routine_instruction_generator(model, iterations, seed, degree, mode, sym
     
     if mode != "invisible":
         print(f"""
-8888888b.  8888888 .d8888b.  
-888   Y88b   888  d88P  Y88b 
-888    888   888  888    888 
-888   d88P   888  888        
-8888888P"    888  888  88888 
-888 T88b     888  888    888 
-888  T88b    888  Y88b  d88P 
-888   T88b 8888888 "Y8888P88
+       d8888 8888888 .d8888b.  
+      d88888   888  d88P  Y88b 
+     d88P888   888  888    888 
+    d88P 888   888  888        
+   d88P  888   888  888  88888 
+  d88P   888   888  888    888 
+ d8888888888   888  Y88b  d88P 
+d88P     888 8888888 "Y8888P88
 
---------- Settings ---------
+---------- Settings ----------
           
 Model:      {model}
 Output:     {output}
@@ -96,7 +96,7 @@ Iterations: {iterations}
 Seed:       {seed}
 Mode:       {mode}
 Symptoms:   {symptoms_string}
-____________________________\n""")
+______________________________\n""")
 
     # Import and convert tina pnml into internal petri net model
     tree = import_petri_net_from_pnml(model)

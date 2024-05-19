c7d469126d1c41c060f6cae2222156efe104915d
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2024-05-19 21:03:27 +0200

    Made output dynamic with carriage return

--- simcoor.py
+++ simcoor.py
@@ -11,38 +11,34 @@ import sys # used for pm4py print suppression
 import io # used for pm4py print suppression
 
 
+
 # USER SETTINGS ########################################################################################################################################
 
-NUMBER_OF_EVENT_LOGS = 1 # INPUT: The number of symptomatic event logs produced (integer >=1)
-NUMBER_OF_CASES = 2 # INPUT: The number of cases in each event log (integer >=1)
-                                                     
-#SimCoor
+NUMBER_OF_EVENT_LOGS = 100 # INPUT: The number of symptomatic event logs produced (integer >=1)
+NUMBER_OF_CASES = 1000 # INPUT: The number of cases in each event log (integer >=1)
 SIMCOOR_SEED = None # INPUT: SimCoor seed (Integer or None value (None value makes seed based on system time))
 OUTPUT_XES = "dementia" # OUTPUT: prefix-naming of the produced event logs (without file format extension)
-
-#AIG
 ROUTINE_MODEL = "morning_routine_template_entitysensors_delay_4.0.pnml" # INPUT: The asymptomatic routine model (PNML)
 SYMPTOMS = ["wandering", "forgetfulness", "repetitiveness_2"] # INPUT: The set of symptoms that one wants to inject (see available options in _Symptoms.py)
-
-# Linac input file paths
 FLOORPLAN_PATH = "morning_routine_floorplan_entitysensors.json" # INPUT: Linac floorplan (JSON)
 LINAC_SETTINGS_PATH = "simulator.json" # INPUT: Linac simulation settings (JSON)
-    
+
+# DEVELOPER SETTINGS ###################################################################################################################################
+
+CSV_PATH = "linac-backend/eventlog.csv" # INTERMEDIARY-FILE: Linac sensor readings (CSV)
+AGENT_INSTRUCTIONS_PATH = "aig-output.json" # INTERMEDIARY-FILE: agent instructions output by the AIG and used as input for Linac (JSON)
+AIG_MODE = "invisible" # INPUT: Possible values: debug, normal, fast, invisible
+SIMCOOR_MODE = "normal" # INPUT: SimCoor print options (possible values: debug, normal, invisible)
 
 # MAIN CODE ############################################################################################################################################
 
-# Developer settings
-CSV_PATH = "linac-backend/eventlog.csv"                                     # INTERMEDIARY-FILE: Linac sensor readings (CSV)
-AGENT_INSTRUCTIONS_PATH = "aig-output.json"                                 # INTERMEDIARY-FILE: agent instructions output by the AIG and used as input for Linac (JSON)
-AIG_MODE = "invisible"                                                      # INPUT: Possible values: debug, normal, fast, invisible
-SIMCOOR_MODE = "normal"                                                     # INPUT: SimCoor print options (possible values: debug, normal, invisible)
-
-# Make random seed if none is specified
-if SIMCOOR_SEED == None:
+# Seed
+if SIMCOOR_SEED == None: # Make random seed if none is specified
     SIMCOOR_SEED = random.randint(0, 2**32 - 1)
+random.seed(SIMCOOR_SEED) # Set random seed value
 
-# Set SimCoor seed
-random.seed(SIMCOOR_SEED)
+# Derive degree increments
+degree_increment = 1 / NUMBER_OF_EVENT_LOGS
 
 # Initial print
 print(f"""                                                                              
@@ -57,21 +53,21 @@ Seed: {SIMCOOR_SEED}
 Event log prefix: {OUTPUT_XES}
 ___________________________\n""")
 
-degree_increment = 1 / NUMBER_OF_EVENT_LOGS
-
+### EVENT LOGIC LOOP
 current_event_log = 1
 degree = 0
 while (degree <= 1):  
+
     # Prints when starting the creation of a new event log
     print(f"""*** Event log {current_event_log} of {NUMBER_OF_EVENT_LOGS+1}""")
 
-    # Cleanup old Linac output CSV file in case of previous SimCoor run halting
+    # Cleanup old intermediary files in case of previous SimCoor run halting
     if os.path.exists(CSV_PATH):
         os.remove(CSV_PATH) # Remove intermediate Linac output
     if os.path.exists(AGENT_INSTRUCTIONS_PATH):
         os.remove(AGENT_INSTRUCTIONS_PATH) # Remove intermediate AIG output
 
-    # Send environment
+    # Send floorplan to Linac backend
     f = open(FLOORPLAN_PATH)
     floorplan = json.load(f)
     f.close()
@@ -82,17 +78,17 @@ while (degree <= 1):
     if SIMCOOR_MODE in ("debug"):
         print(floorplanreq.text+"\n")
 
-
-    for x in range(1,NUMBER_OF_CASES+1):
+    ### CASE LOGIC LOOP
+    for case in range(1,NUMBER_OF_CASES+1):
         if SIMCOOR_MODE in ("debug", "normal"):
-            print(f"* Case {x} of {NUMBER_OF_CASES}") #, end='\r'
+            print(f"* Case {case} of {NUMBER_OF_CASES}", end="\r") #, end='\r'
 
         # Generate routine instructions by running the AIG
         if SIMCOOR_MODE in ("debug"):
             print("Generate agent instructions\n")
         agent_instructions_generator.aig.run_routine_instruction_generator(ROUTINE_MODEL, 1, random.random(), degree, AIG_MODE, SYMPTOMS, AGENT_INSTRUCTIONS_PATH, FLOORPLAN_PATH)
 
-        # Send agent instructions
+        # Send agent instructions to Linac
         f = open(AGENT_INSTRUCTIONS_PATH)
         inputFile = json.load(f)
         f.close()
@@ -103,17 +99,19 @@ while (degree <= 1):
         if SIMCOOR_MODE in ("debug"):
             print(inputreq.text+"\n")
 
-        # change caseID and send simulator settings
+        # Change caseID and seed value before sending simulator settings to Linac
         f = open(LINAC_SETTINGS_PATH)
         simulator = json.load(f)
         f.close()
-        simulator["caseID"] = x
+        simulator["caseID"] = case
         simulator["seed"] = random.random()
         if SIMCOOR_MODE in ("debug"):
             print("sending simulator settings")
         simulatorreq = requests.post("http://localhost:8080/api/simulation/simulator", json = simulator)
         if SIMCOOR_MODE in ("debug"):
             print(simulatorreq.text+"\n")
+
+    ### EVENT LOG CONVERSION
 
     # Convert CSV to XES
     dataframe = pd.read_csv(CSV_PATH, sep=',', header=None)
@@ -168,10 +166,13 @@ while (degree <= 1):
     if SIMCOOR_MODE in ("debug", "normal"):
         print(f"Exported event log: \"{OUTPUT_XES}_(C{NUMBER_OF_CASES}-D{degree:.{3}f}).xes\"\n")
 
+    ### EVENT LOG LOOP END OPERATIONS
+
     # Cleanup
     os.remove(CSV_PATH) # Remove intermediate Linac output
     os.remove(AGENT_INSTRUCTIONS_PATH) # Remove intermediate AIG output
 
+    # update of loop variables
     current_event_log += 1
     degree += degree_increment
-    degree = round(degree, 3)
+    degree = round(degree, 6)

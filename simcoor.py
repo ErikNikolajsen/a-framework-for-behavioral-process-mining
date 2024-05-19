880786324b1fa97c6c08e0058dd3951c9d4fe97f
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2024-05-19 19:29:17 +0200

    Derived degree granularity from number of events

--- simcoor.py
+++ simcoor.py
@@ -7,26 +7,26 @@ import os
 import random
 import warnings
 #import copy
+import sys # used for pm4py print suppression
+import io # used for pm4py print suppression
 
 
 # USER SETTINGS ########################################################################################################################################
 
-NUMBER_OF_EVENT_LOGS = 11                                                       # INPUT: The number of symptomatic event logs produced (integer >=1)
-EVENT_LOGS = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]            # INPUT: Each element represent the creation of an event log with the value of the element representing the degree of evolution
+NUMBER_OF_EVENT_LOGS = 1 # INPUT: The number of symptomatic event logs produced (integer >=1)
+NUMBER_OF_CASES = 2 # INPUT: The number of cases in each event log (integer >=1)
                                                      
 #SimCoor
-SIMCOOR_MODE = "normal"                                                     # INPUT: SimCoor print options (possible values: debug, normal, invisible)
-SIMCOOR_SEED = None                                                          # INPUT: SimCoor seed - None value makes seed based on system time 
-OUTPUT_XES = "wandering_forgetfulness_repetitiveness_2_100"        # OUTPUT: naming of the produced event logs (without file format extension)
+SIMCOOR_SEED = None # INPUT: SimCoor seed (Integer or None value (None value makes seed based on system time))
+OUTPUT_XES = "dementia" # OUTPUT: naming of the produced event logs (without file format extension)
 
 #AIG
-ROUTINE_MODEL = "morning_routine_template_entitysensors_delay_4.0.pnml"     # INPUT: The asymptomatic routine model (PNML)
-NUMBER_OF_CASES = 2                                                             # INPUT: The number of cases in each event log (integer >=1)
-SYMPTOMS = ["wandering", "forgetfulness", "repetitiveness_2"]               # INPUT: The set of symptoms that one wants to inject (see available options in _Symptoms.py)
+ROUTINE_MODEL = "morning_routine_template_entitysensors_delay_4.0.pnml" # INPUT: The asymptomatic routine model (PNML)
+SYMPTOMS = ["wandering", "forgetfulness", "repetitiveness_2"] # INPUT: The set of symptoms that one wants to inject (see available options in _Symptoms.py)
 
 # Linac input file paths
-FLOORPLAN_PATH = "morning_routine_floorplan_entitysensors.json"             # INPUT: Linac floorplan (JSON)
-LINAC_SETTINGS_PATH = "simulator.json"                                            # INPUT: Linac simulation settings (JSON)
+FLOORPLAN_PATH = "morning_routine_floorplan_entitysensors.json" # INPUT: Linac floorplan (JSON)
+LINAC_SETTINGS_PATH = "simulator.json" # INPUT: Linac simulation settings (JSON)
     
 
 # MAIN CODE ############################################################################################################################################
@@ -35,6 +35,7 @@ LINAC_SETTINGS_PATH = "simulator.json"                                          
 CSV_PATH = "linac-backend/eventlog.csv"                                     # INTERMEDIARY-FILE: Linac sensor readings (CSV)
 AGENT_INSTRUCTIONS_PATH = "aig-output.json"                                 # INTERMEDIARY-FILE: agent instructions output by the AIG and used as input for Linac (JSON)
 AIG_MODE = "invisible"                                                      # INPUT: Possible values: debug, normal, fast, invisible
+SIMCOOR_MODE = "normal"                                                     # INPUT: SimCoor print options (possible values: debug, normal, invisible)
 
 # Make random seed if none is specified
 if SIMCOOR_SEED == None:
@@ -43,6 +44,7 @@ if SIMCOOR_SEED == None:
 # Set SimCoor seed
 random.seed(SIMCOOR_SEED)
 
+
 # Initial print
 print(f"""                                                                              
   __          _            
@@ -50,16 +52,19 @@ print(f"""
  __) | | | | \_ (_) (_) |
             
 --------- Settings --------
-Mode: {SIMCOOR_MODE}
 Seed: {SIMCOOR_SEED}
+Event logs: {NUMBER_OF_EVENT_LOGS}
+Cases: {NUMBER_OF_CASES}
+
 ___________________________\n""")
 
-for i in range(0,NUMBER_OF_EVENT_LOGS):  
+degree_increment = 1 / NUMBER_OF_EVENT_LOGS
 
-    DEGREE = 0.1*i                                                              # INPUT: Degree of symptom evolution
-
+current_event_log = 1
+degree = 0
+while (degree <= 1):  
     # Prints when starting the creation of a new event log
-    print(f"""*** Event log {i} of {NUMBER_OF_EVENT_LOGS}""")
+    print(f"""*** Event log {current_event_log} of {NUMBER_OF_EVENT_LOGS+1}""")
 
     # Cleanup old Linac output CSV file in case of previous SimCoor run halting
     if os.path.exists(CSV_PATH):
@@ -81,12 +86,12 @@ for i in range(0,NUMBER_OF_EVENT_LOGS):
 
     for x in range(1,NUMBER_OF_CASES+1):
         if SIMCOOR_MODE in ("debug", "normal"):
-            print(f"* Degree {DEGREE} - Trace {x}") #, end='\r'
+            print(f"* Case {x} of {NUMBER_OF_CASES}") #, end='\r'
 
         # Generate routine instructions by running the AIG
         if SIMCOOR_MODE in ("debug"):
             print("Generate agent instructions\n")
-        agent_instructions_generator.aig.run_routine_instruction_generator(ROUTINE_MODEL, 1, random.random(), DEGREE, AIG_MODE, SYMPTOMS, AGENT_INSTRUCTIONS_PATH, FLOORPLAN_PATH)
+        agent_instructions_generator.aig.run_routine_instruction_generator(ROUTINE_MODEL, 1, random.random(), degree, AIG_MODE, SYMPTOMS, AGENT_INSTRUCTIONS_PATH, FLOORPLAN_PATH)
 
         # Send agent instructions
         f = open(AGENT_INSTRUCTIONS_PATH)
@@ -149,15 +154,24 @@ for i in range(0,NUMBER_OF_EVENT_LOGS):
 
 
     dataframe = pm4py.format_dataframe(dataframe, case_id='case:concept:name', activity_key='sensor:reading', timestamp_key='time:timestamp', timest_format = '%Y-%m-%dT%H:%M:%S.%f%z')
+    
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         event_log = pm4py.convert_to_event_log(dataframe)
-    pm4py.write_xes(event_log, OUTPUT_XES+f"_({i}).xes")
+
+
+    original_stderr = sys.stderr # used for suppressing pm4py output
+    sys.stderr = io.StringIO() # used for suppressing pm4py output
+    pm4py.write_xes(event_log, OUTPUT_XES+f"_({degree}).xes")
+    sys.stderr = original_stderr # used for suppressing pm4py output
 
     if SIMCOOR_MODE in ("debug", "normal"):
-            print(f"Event log exported: {OUTPUT_XES}_({i}).xes\n") #, end='\r'
-
+        print(f"Exported event log: \"{OUTPUT_XES}_(C{NUMBER_OF_CASES}_D{degree:.{3}f}).xes\"\n")
 
     # Cleanup
     os.remove(CSV_PATH) # Remove intermediate Linac output
     os.remove(AGENT_INSTRUCTIONS_PATH) # Remove intermediate AIG output
+
+    current_event_log += 1
+    degree += degree_increment
+    degree = round(degree, 3)
